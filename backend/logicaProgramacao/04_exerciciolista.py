e7d9c931c7b7f5e9d166cad959d5331209c56591
Guilherme <guiatinoco@gmail.com>
2025-08-19 09:44:04 -0300

    meu pai

---
+++ backend/logicaProgramacao/04_exerciciolista.py
@@ -0,0 +1,20 @@
+#crie uma lista de taredas
+#adicione 5 tarefas na lista
+#adicione uma tarefa na posição 2 da lista
+#remova a tarefa "lavar louça" da lista
+#remover a tarefa da posição 1 da lista
+
+tarefas = [
+    "arrumar a casa"
+    "fazer compras"
+    "limpar a casa"
+    "lavar moto"
+    "lavar louca"
+
+]
+
+tarefas.insert(2, "Trabalhar")
+
+tarefas.remove("lavar louça")
+
+
